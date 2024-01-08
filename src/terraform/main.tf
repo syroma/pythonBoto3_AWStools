b861fedbab38c10ea40f7a0756c5217cd056f192
Sy Roman <osiris.roman@protonmail.com>
2024-01-08 13:34:34 -0600

    testing running from AzDo

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -1,3 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    http = {
+      source  = "hashicorp/http"
+      version = "3.4.0"
+    }
+  }
+  backend "s3" {
+    bucket = "terraform-sy"
+    key    = "tf/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
 provider "aws" {
   region = "us-east-1"
 }
--- src/terraform/provider.tf
+++ src/terraform/provider.tf
@@ -1,17 +1,17 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-    http = {
-      source  = "hashicorp/http"
-      version = "3.4.0"
-    }
-  }
-  backend "s3" {
-    bucket = "terraform-sy"
-    key    = "tf/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
+# terraform {
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "~> 5.0"
+#     }
+#     http = {
+#       source  = "hashicorp/http"
+#       version = "3.4.0"
+#     }
+#   }
+#   backend "s3" {
+#     bucket = "terraform-sy"
+#     key    = "tf/terraform.tfstate"
+#     region = "us-east-1"
+#   }
+# }
